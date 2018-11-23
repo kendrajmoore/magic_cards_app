918b539c1dea29c85e48d54174ae416add920537
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2018-11-22 18:27:39 -0800

    update links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :card_data
   devise_for :users
-    get 'card/index'
     root 'home#index'
-    get 'home/about'
-    get 'home/search'
-    get 'home/show'
+    get 'card/index', to: 'card#index'
+    get 'home/about', to: 'home#about'
+    get 'home/search', to: 'home#search'
+    get 'home/show', to: 'home#show'
     post 'home/search' => 'home/search'
 end
