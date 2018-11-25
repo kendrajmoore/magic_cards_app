c642f0d135d964a488b59e43364919826d2700d0
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2018-11-24 20:10:51 -0800

    update puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,23 +1,8 @@
-threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
- threads threads_count, threads_count
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
- environment ENV.fetch('RAILS_ENV', 'development')
- port        ENV.fetch('PORT', 3000)
-
- workers ENV.fetch('WEB_CONCURRENCY', 2)
-
- preload_app!
-
- before_fork do
-   ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
- end
-
- on_worker_boot do
-   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
- end
-
- # Allow puma to be restarted by `rails restart` command.
- plugin :tmp_restart
+preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
